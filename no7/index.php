a4c187011e61bab5a8fffdc5b0fe4e1588e27ba0
aderifqi <aderifqi85@gmail.com>
2021-06-28 21:00:19 +0700

    remove file update.php

--- no7/index.php
+++ no7/index.php
@@ -1,13 +1,17 @@
 <?php 
 require 'function.php';
+$input= 'submit';
+$readonly = false;
 if (isset($_GET["npm"])){
   $npm = $_GET["npm"];
 
   $sql = "SELECT * FROM mahasiswa WHERE npm = $npm";
   $row = $conn->query($sql)->fetch(PDO::FETCH_ASSOC);
+  $input = 'update';
+  $readonly = true;
 }
 if(isset($_POST["submit"])){
-// cek apakah data berhasil di tambahkan atau tidak
+  // cek apakah data berhasil di tambahkan atau tidak
   if( addData($_POST) > 0 ) {
     echo "
       <script>
@@ -23,6 +27,23 @@ if(isset($_POST["submit"])){
       </script>
     ";
   }
+}elseif(isset($_POST["update"])){
+	// cek apakah data berhasil diubah atau tidak
+	if( updateData($_POST) > 0 ) {
+		echo "
+			<script>
+				alert('data berhasil diubah!');
+				document.location.href = 'index.php';
+			</script>
+		";
+	} else {
+		echo "
+			<script>
+				alert('data gagal diubah!');
+				document.location.href = 'index.php';
+			</script>
+		";
+	}
 }
 
 ?>
@@ -57,12 +78,12 @@ tr:nth-child(even) {
 
   <form action="" method="POST">
     <label for="npm">Npm</label>
-    <input type="text" id="npm" name="npm" value="<?= (isset($row)) ? $row['npm'] : ''; ?>" readonly>
+    <input type="text" id="npm" name="npm" value="<?= (isset($row)) ? $row['npm'] : ''; ?>" <?= ($readonly) ? 'readonly' : ''; ?> >
     <label for="nama">Nama</label>
-    <input type="text" id="nama" name="nama" value="<?= $row['nama'] ?>">
+    <input type="text" id="nama" name="nama" value="<?= (isset($row)) ? $row['nama'] : ''; ?>">
     <label for="alamat">Alamat</label>
-    <input type="text" id="alamat" name="alamat" value="<?= $row['alamat'] ?>">
-    <button type="submit" name="submit">Update Data</button>
+    <input type="text" id="alamat" name="alamat" value="<?= (isset($row)) ? $row['alamat'] : ''; ?>">
+    <button type="submit" name="<?= $input ?>">Submit</button>
   </form>
 <br>
 
